8db2e76e8d6603107fc51084f7c9217feb88a756
Jonathan George <102447711+jgeorge2825@users.noreply.github.com>
2022-03-26 22:16:32 -0400

    Create mycp.h
    Standard header file

---
+++ mycp.h
@@ -0,0 +1,16 @@
+// standard header file
+//
+#ifndef ISIP_EXAMPLE
+#define ISIP_EXAMPLE
+
+// system include files
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <cstring>
+
+// end of include file
+//
+#endif
